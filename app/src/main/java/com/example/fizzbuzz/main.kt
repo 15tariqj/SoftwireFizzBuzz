0b3209acc51177fcb4e2512c7d9f14c6b5389fd5
Tariq Jamal <tarjam@treefrog.zoo.lan>
2021-06-22 11:34:45 +0100

    Users can input max number

--- app/src/main/java/com/example/fizzbuzz/main.kt
+++ app/src/main/java/com/example/fizzbuzz/main.kt
@@ -3,21 +3,27 @@ package com.example.fizzbuzz
 import kotlin.text.*
 
 fun main() {
-    for (x in 1..300) {
+    print("Please enter the maximum number that outputs should be printed to: ")
+    val y = readLine()!!.toInt()
+    print("Please enter the numbers for the rules you wish to enable, seperated by commas: ")
+    var z = readLine()!!
+    var zInts = (z.split(",").toTypedArray()).map { it.toInt() }.toTypedArray()
+    println(zInts[2])
+    for (x in 1..y) {
         var res = ""
-        if (x % 3 == 0)
+        if (x % 3 == 0 && zInts.contains(3))
             res += "Fizz"
-        if (x % 13 == 0) res += "Fezz"
-        if (x % 11 == 0) {
+        if (x % 13 == 0 && zInts.contains(13)) res += "Fezz"
+        if (x % 11 == 0 && zInts.contains(11)) {
             res = "Bong"
-            if (x % 13 == 0) res = "Fezz$res"
-            if (x % 17 == 0) res = reverse(res)
+            if (x % 13 == 0 && zInts.contains(13)) res = "Fezz$res"
+            if (x % 17 == 0 && zInts.contains(17)) res = reverse(res)
             println(res)
             continue
         }
-        if (x % 5 == 0) res += "Buzz"
-        if (x % 7 == 0) res += "Bang"
-        if (x % 17 == 0) res = reverse(res)
+        if (x % 5 == 0 && zInts.contains(5)) res += "Buzz"
+        if (x % 7 == 0 && zInts.contains(7)) res += "Bang"
+        if (x % 17 == 0 && zInts.contains(17)) res = reverse(res)
         if (res == "") println(x)
         else println(res)
     }
