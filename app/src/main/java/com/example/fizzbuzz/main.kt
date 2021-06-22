e30bf04cc141286dba8aa22cdcc3a1b0fe2a4d7f
Tariq Jamal <tarjam@treefrog.zoo.lan>
2021-06-22 10:59:27 +0100

    Completed up-to 'Bong'

--- app/src/main/java/com/example/fizzbuzz/main.kt
+++ app/src/main/java/com/example/fizzbuzz/main.kt
@@ -1,5 +1,16 @@
 package com.example.fizzbuzz
 
+import kotlin.text.*
+
 fun main() {
-    print("Hello World!")
+    for (x in 1..100) {
+        var res = ""
+        if (x % 3 == 0)
+            res += "Fizz"
+        if (x % 5 == 0) res += "Buzz"
+        if (x % 7 == 0) res += "Bang"
+        if (x % 11 == 0) res = "Bong"
+        if (res == "") println(x)
+        else println(res)
+    }
 }
