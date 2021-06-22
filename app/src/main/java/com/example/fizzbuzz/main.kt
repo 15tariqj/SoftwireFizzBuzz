296eb96bceaa1ab38e7269b40bb41ea22b2a6471
Tariq Jamal <tarjam@treefrog.zoo.lan>
2021-06-22 11:15:29 +0100

    Completed up to reverse order

--- app/src/main/java/com/example/fizzbuzz/main.kt
+++ app/src/main/java/com/example/fizzbuzz/main.kt
@@ -3,14 +3,29 @@ package com.example.fizzbuzz
 import kotlin.text.*
 
 fun main() {
-    for (x in 1..100) {
+    for (x in 1..300) {
         var res = ""
         if (x % 3 == 0)
             res += "Fizz"
+        if (x % 13 == 0) res += "Fezz"
+        if (x % 11 == 0) {
+            res = "Bong"
+            if (x % 13 == 0) res = "Fezz$res"
+            if (x % 17 == 0) res = reverse(res)
+            println(res)
+            continue
+        }
         if (x % 5 == 0) res += "Buzz"
         if (x % 7 == 0) res += "Bang"
-        if (x % 11 == 0) res = "Bong"
+        if (x % 17 == 0) res = reverse(res)
         if (res == "") println(x)
         else println(res)
     }
+}
+
+fun reverse(a: String): String {
+    var chunks: List<String> = a.chunked(4)
+    chunks = chunks.reversed()
+    val res = chunks.joinToString("")
+    return res
 }
